c1cc2d21536aa180910ea545da1ad77be94fdc7b
mauro <mauro@lacy.com.ar>
2013-06-11 13:47:21 -0300

    Needs root added.

--- extra/parport0.sh
+++ extra/parport0.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+ID=`id -u`
+[ $ID -ne 0 ] && echo "Needs root." && exit 1
+
 P=/dev/parport0
 
 echo -n "Installing device..."
-sudo mknod $P c 99 0
-sudo chgrp lp $P
-sudo chmod g+rw $P
+mknod $P c 99 0
+chgrp lp $P
+chmod g+rw $P
 echo "done."
-echo "Make sure user '$USER' is in group 'lp'."
+echo "Make sure user '$SUDO_USER' is in group 'lp'."
