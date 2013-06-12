bd1bbccde1066087aaa4e185a2f35cc94064b8eb
mauro <mauro@lacy.com.ar>
2013-06-11 21:04:31 -0300

    All four axes (X and Z tested)

--- pointer.py
+++ pointer.py
@@ -16,14 +16,6 @@ AXIS_Y	= 1
 AXIS_Z	= 2
 AXIS_A	= 3
 
-PINS=((17, 16), (14, 1), (2, 3), (4, 5)) # (STEP, DIR) pins for earch axis
-
-PORT_DATA=0
-PORT_CTRL=1
-PORT_STAT=2 # Not used
-PORT=(-1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 1, 2, 1, 1) # port(data/ctrl/status) for each pin
-# Data bit for earch pin (DATA, CTRL, STAT)
-BIT =((-1, -1, 0, 1, 2, 3, 4, 5, 6, 7), (-1,  0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 2, 3), (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 6, 7, 5, 4, -1, 3))
 
 class EightBitIO(object):
     def __init__(self):
@@ -52,47 +44,79 @@ class EightBitIO(object):
         self.data = data
         self.p.setData(self.data)
 
-#    def putc(self, c):
-#        """send a data byte to the LCD"""
-#        self.setRS(1)
-#        self.setRW(0)
-#        self.out(ord(c))
-#        self.toggleE()              #toggle LCD_E, the enable pin
-#        time.sleep(0.001)           #wait until instr is finished
-
-
 class Pointer(EightBitIO):
     def __init__(self):
         self.p = parallel.Parallel()
+        
+        self.sleep_ON  = .005
+        self.sleep_OFF = .005
+        self.PINS = ((17, 16), (14, 1), (2, 3), (4, 5)) # (STEP, DIR) pins for earch axis      
+        
+        self.PORT_DATA=0
+        self.PORT_CTRL=1
+        self.PORT_STAT=2 # Not used
+        # Port(DATA/CTRL) for each pin
+        self.PORT     = { 1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 2, 11: 2, 12: 2, 13: 2, 14: 1, 15: 2, 16: 1, 17: 1}
+        
+        # Data bit for each Data pin
+        self.DATA_BIT = lambda pin: pin-2
+        
+        # Control function for each Control pin
+        self.CTRL_FUN = {1: self.p.setDataStrobe, 14: self.p.setAutoFeed, 16: self.p.setInitOut, 17: self.p.setSelect}
+        
         super(Pointer, self).__init__()
         
     def move(self, axis, steps=STEP_ONE, dir=DIR_CW):
         """Move 'axis' a 'steps' number of steps in direction 'dir'"""
         if (AXIS_X <= axis <= AXIS_A) and steps >0 and dir in (DIR_CW, DIR_CCW):
-            pins=PINS[axis]
-            port=PORT[pins[0]] # It's the same por for both pins
-            bits=(BIT[port][pins[0]], BIT[port][pins[1]])
-            data=(1<<bits[0]) | (dir<<(bits[1]))
+            pins=self.PINS[axis]
+            port=self.PORT[pins[0]] # The same port for both pins
 
-            i=1
-            print "Axis: %d, Steps: %d, Dir: %d" % (axis, steps, dir)
-            while steps > 0:
-                print "step %d: port %d, axis bit  %d, dir bit  %d, data 0x%02x" % (i, port, bits[0], bits[1], data)
-                if port == PORT_DATA:
+            if port == self.PORT_DATA:
+                bits=(self.DATA_BIT(pins[0]), self.DATA_BIT(pins[1]))
+                data=(1<<bits[0]) | (dir<<(bits[1]))
+                for i in range(steps):
+                    print "step", i+1
                     self.out(data)
-                steps -= 1
-                i += 1
-            print
+                    time.sleep(self.sleep_ON)
+                    self.out(0)
+                    time.sleep(self.sleep_OFF)
+            elif port == self.PORT_CTRL:
+                stepFunction = self.CTRL_FUN[pins[0]]
+                dirFunction  = self.CTRL_FUN[pins[1]]
+                # set dir first
+                dirFunction(dir)
+                # set step(s)
+                for i in range(steps):
+                    print "step", i+1
+                    stepFunction(1)
+                    time.sleep(self.sleep_ON)
+                    stepFunction(0)
+                    time.sleep(self.sleep_OFF)
+            else:
+                print "ERROR: axis %d steps %d dir %d" %(axis, steps, dir)
         else:
             print "ERROR: axis %d steps %d dir %d" %(axis, steps, dir)
 
 if __name__ == '__main__':
     pointer = Pointer()
-    pointer.move(AXIS_X, STEP_ONE, DIR_CW)
-    pointer.move(AXIS_X, STEP_ONE, DIR_CCW)
-    pointer.move(AXIS_Y, STEP_ONE, DIR_CW)
-    pointer.move(AXIS_Y, STEP_ONE, DIR_CCW)
-    pointer.move(AXIS_Z, STEP_ONE, DIR_CW)
-    pointer.move(AXIS_Z, STEP_ONE, DIR_CCW)
-    pointer.move(AXIS_A, STEP_ONE, DIR_CW)
-    pointer.move(AXIS_A, STEP_ONE, DIR_CCW)
+#    pointer.move(AXIS_X, STEP_ONE, DIR_CW)
+#    pointer.move(AXIS_X, STEP_ONE, DIR_CCW)
+#    pointer.move(AXIS_Y, STEP_ONE, DIR_CW)
+#    pointer.move(AXIS_Y, STEP_ONE, DIR_CCW)
+#    pointer.move(AXIS_Z, STEP_ONE, DIR_CW)
+#    pointer.move(AXIS_Z, STEP_ONE, DIR_CCW)
+#    pointer.move(AXIS_A, STEP_ONE, DIR_CW)
+#    pointer.move(AXIS_A, STEP_ONE, DIR_CCW)
+
+    if len(sys.argv) != 4 or int(sys.argv[2]) <= 0 or sys.argv[3] not in ('CW', 'CCW'):
+        print >>sys.stderr, "Usage: %s <axis> <steps> <dir>\naxis : {X|Y|Z|A}\nsteps: Number of steps\ndir  : {CW|CCW}" % sys.argv[0]
+        sys.exit(1)
+    axis = {'X': AXIS_X, 'Y': AXIS_Y, 'Z': AXIS_Z, 'A': AXIS_A}
+    dirs = {'CW': DIR_CW, 'CCW': DIR_CCW}
+    axis = axis[sys.argv[1]]
+    steps=  int(sys.argv[2])
+    dir  = dirs[sys.argv[3]]
+
+    print "axis: %s, dir: %s, steps: %d" % (sys.argv[1], sys.argv[3], steps)
+    pointer.move(axis, steps, dir)
