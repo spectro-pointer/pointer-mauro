50445a4451977f3c6b2bad3a7e15024b8d69673b
mauro <mauro@lacy.com.ar>
2013-08-10 20:37:29 -0300

    Accelerator driver.

---
+++ accelerator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+ 
+class Accelerometer(object):
+    """ Accelerometer Driver
+        ADXL345 Accelerometer
+    """
+    
+    def __init__(self):
+        from i2clibraries import i2c_adxl345
+        self.adxl345 = i2c_adxl345.i2c_adxl345(1)
+        self.adxl345.setScale(16) # 2, 4, 8, 16
+ 
+    def getCoords(self):
+        self.adxl345.wakeUp()
+#        return self.adxl345.getRawAxes()
+        return self.adxl345.getAxes()
