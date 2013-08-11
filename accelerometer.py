02cf5909cfb824749ce1aaba58a11bd245f6488d
Mauro Lacy <mauro@lacy.com.ar>
2013-08-10 22:13:07 -0300

    Highres scale. Renamed method. Added main loop

--- accelerometer.py
+++ accelerometer.py
@@ -1,17 +1,26 @@
-#!/usr/bin/python3
-
- 
+#!/usr/bin/python3 
 class Accelerometer(object):
     """ Accelerometer Driver
         ADXL345 Accelerometer Class driver
     """
-    
     def __init__(self):
         from i2clibraries import i2c_adxl345
         self.adxl345 = i2c_adxl345.i2c_adxl345(1)
-        self.adxl345.setScale(16) # 2, 4, 8, 16
+        self.scale = 2 # 2, 4, 8, 16
+        self.adxl345.setScale(self.scale)
  
-    def getCoords(self):
+    def getAxes(self):
+        """ Get X,Y,Z accelerations
+        """
         self.adxl345.wakeUp()
 #        return self.adxl345.getRawAxes()
         return self.adxl345.getAxes()
+
+from time import sleep
+
+if __name__ == '__main__':
+    accel = Accelerometer()
+
+    while True:
+        print(accel.getAxes())
+        sleep(1)
