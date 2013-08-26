2e3f7d13ce278161c522b38a2c2a39d3157b0040
mauro <mauro@lacy.com.ar>
2013-08-25 22:58:52 -0300

    Angles (Elevation) Sensor using Accelerometer.

--- accelerometer.py
+++ accelerometer.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+
 class Accelerometer(object):
     """ Accelerometer Driver
         ADXL345 Accelerometer Class driver
@@ -16,11 +17,41 @@ class Accelerometer(object):
 #        return self.adxl345.getRawAxes()
         return self.adxl345.getAxes()
 
+import numpy
+import math
+class AnglesSensor(Accelerometer):
+    """ Angles converter
+        Uses Accelerometer to Report (Approximate) Inclination Angles
+    """
+    def __init__(self):
+        """Orientation of Accelerometer wrt Pointer Body axes 
+        """
+        self.accelOrientation = numpy.array([1., -1., -1.])
+        """ Angles Conversion""" 
+        self.Angles = numpy.array([90./1., 90./1., 90/1.])
+        
+        super(AnglesSensor, self).__init__()
+        
+    def get(self):
+        """ Use the accelerometer to (try to) measure angles
+            Angles must be read when the device is not moving
+        """
+        
+        axes = numpy.array(self.getAxes())
+        axes /= numpy.linalg.norm(axes)
+        print ('axes:', axes)
+        axes[0] = math.degrees(math.acos(axes[0]))
+#        if axes[2] > 0.:
+        if axes[2]-axes[1] > 0.:
+            axes[0] = 360. -axes[0]
+        return list(axes)
+    
 from time import sleep
-
 if __name__ == '__main__':
-    accel = Accelerometer()
+#    accel = Accelerometer()
+    angles = AnglesSensor()
 
     while True:
-        print(accel.getAxes())
+#        print(accel.getAxes())
+        print(angles.get())
         sleep(1)
