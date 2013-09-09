4bff5f1b210550b62b6b0d699ab51fb63cced56e
mauro <mauro@lacy.com.ar>
2013-09-08 21:49:52 -0300

    gps poller client class.

---
+++ gps_client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+from util import Thread
+import time
+from gps import *
+
+
+class GpsPoller(Thread):
+
+    def __init__(self, server='localhost'):
+        Thread.__init__(self)
+#        self.setDaemon(True)
+        self.session = gps(host=server, mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+        self.current_value = None
+
+    def get(self):
+        return self.current_value
+
+    def run(self):
+        try:
+            while not self.shallStop:
+                self.current_value = next(self.session)
+        except StopIteration:
+            pass
+
+if __name__ == '__main__':
+    g = GpsPoller(server='pi')
+    g.start()
+    while True:
+        try:
+            time.sleep(1)
+            gpsData = g.get()
+#            print(gpsData)
+            for k in gpsData.keys():
+                print("%s: %s" % (k, gpsData[k]))
+            print()
+            print(gpsData['lat'], gpsData['lon'])
+        except KeyError:
+            pass
+        except (KeyboardInterrupt, SystemExit):
+            g.shutdown()
+            break
+    print()
