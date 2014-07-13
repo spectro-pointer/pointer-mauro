2a46cacacc06f44f71ae0d46473958e0317a44e8
Mauro Lacy <mauro@lacy.com.ar>
2014-07-13 19:18:25 -0300

    rtsp sizes.

--- rtsp-opencv/rtsp-server.sh
+++ rtsp-opencv/rtsp-server.sh
@@ -1,3 +1,7 @@
 :
 
-raspivid -n -ih -t 0 -ISO 800 -ex night -w 720 -h 405 -fps 25 -b 200000 -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
+A=16/9
+W=1024
+H=576
+#raspivid -n -ih -t 0 -ISO 800 -ex night -w $W -h $H -fps 25 -b 1000000 -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
+raspivid -n -ih -t 0 -ISO 100 -w $W -h $H -fps 25 -b 100000 -o - | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
