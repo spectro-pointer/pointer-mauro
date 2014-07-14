b1cee56684e01190e92f1167e79dfc268d23b3c1
Mauro Lacy <mauro@lacy.com.ar>
2014-07-14 14:13:36 -0300

    gstreamer0.10 vs. 1.0 support.

--- rtsp-opencv/gs-client.sh
+++ rtsp-opencv/gs-client.sh
@@ -1,7 +1,13 @@
 :
 
 IP="pi"
-#H264_DEC="omxh264dec" # rpi H.264 decoder
-H264_DEC="avdec_h264"
 
-gst-launch-1.0 -v tcpclientsrc host=$IP port=5000 ! gdpdepay ! rtph264depay !h264parse ! $H264_DEC ! videoconvert !autovideosink sync=false
+#H264_DEC="omxh264dec" # Rpi (HW accelerated) H.264 decoder
+
+#GSV=0.10
+#H264_DEC="ffdec_h264" # gstreamer 0.10 H2.64 ffmpeg decoder (gstreamer0.10-ffmpeg)
+
+GSV=1.0
+H264_DEC="h264parse ! avdec_h264 ! videoconvert" # gstreamer 1.0 H.264 decoder pipeline
+
+gst-launch-$GSV -v tcpclientsrc host=$IP port=5000 ! gdpdepay ! rtph264depay ! $H264_DEC ! autovideosink sync=false
