8c953de9eebd416489ce54fdde1d4cf6e2bdde04
Mauro Lacy <mauro@lacy.com.ar>
2014-07-14 13:55:17 -0300

    H.264 decoder for Ubuntu 12.04

--- rtsp-opencv/gs-client.sh
+++ rtsp-opencv/gs-client.sh
@@ -1,5 +1,7 @@
 :
 
 IP="pi"
+#H264_DEC="omxh264dec" # rpi H.264 decoder
+H264_DEC="avdec_h264"
 
-gst-launch-1.0 -v tcpclientsrc host=$IP port=5000 !gdpdepay ! rtph264depay !h264parse ! omxh264dec ! videoconvert !autovideosink sync=false
+gst-launch-1.0 -v tcpclientsrc host=$IP port=5000 ! gdpdepay ! rtph264depay !h264parse ! $H264_DEC ! videoconvert !autovideosink sync=false
