4fed4bfd1cb9701aa5d92a89df3f38edd4b60b74
Mauro Lacy <mauro@lacy.com.ar>
2014-07-16 05:10:22 -0300

    Fixed opencv 3.0 install script.

--- extra/opencv3.0+gstreamer1.0.sh
+++ extra/opencv3.0+gstreamer1.0.sh
@@ -1,8 +1,8 @@
 :
 
 # OpenCV installation for Ubuntu 12.04
-# Installs OpenCV 3.0 on Ubuntu 12.04
-# First install a developer environment to build OpenCV.
+# Installs OpenCV 3.0
+# Install a developer environment to build OpenCV
 sudo apt-get -y install build-essential cmake pkg-config
 
 # Install Image I/O libraries
@@ -27,18 +27,18 @@ sudo apt-get -y install python-dev python-numpy
 # Optional - install the parallel code processing library (the Intel tbb library)
 sudo apt-get -y install libtbb-dev
 
-# install the Qt dev library
+# Install the Qt dev library
 sudo apt-get -y install libqt4-dev
 
 # Now download OpenCV 3.0 to wherever you want to compile the source
 cd ~/src
-sudo rm -rf opencv
-sudo mkdir opencv
-sudo chown $SUDO_USER opencv
-git clone git://github.com/Itseez/opencv.git
+#sudo rm -rf opencv-3.0
+sudo mkdir opencv-3.0
+sudo chown $USER opencv-3.0
+git clone git://github.com/Itseez/opencv.git opencv-3.0
 
 # Create and build directory and onfigure OpenCV with cmake.
-cd opencv
+cd opencv-3.0
 mkdir build
 cd build
 cmake \
@@ -46,7 +46,7 @@ cmake \
     -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON \
     -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF \
     -D BUILD_EXAMPLES=OFF -D WITH_QT=ON -D WITH_OPENGL=ON \
-    -D BUILD_PERF_TESTS=OFF .. # Due to broken CUDA performance tests
+    -D BUILD_PERF_TESTS=OFF .. # CUDA performance tests broken
 
 # Compile it
 make
